55661434e36dc7e67dfd2fead7572b2bd93e839c
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2024-11-03 10:12:28 +0200

    Add edit/save toggle

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function editHandler() {
+        setIsEditing(!isEditing);
+
+    }
+    
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                { !isEditing ? <span className="player-name">{name}</span> : <input value={name} /> }
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={editHandler}>{ isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 }
