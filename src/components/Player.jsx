cdc921ba1363561c289bb8790c13ce13ec4c4d2f
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2024-11-12 12:24:01 +0200

    set player turn

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ name, symbol, isActive }) {
 
     const [ playerName, setPlayerName ] = useState(name)
     const [ isEditing, setIsEditing ] = useState(false);
@@ -16,7 +16,7 @@ export default function Player({ name, symbol }) {
     }
     
     return (
-        <li>
+        <li className={isActive ? "active" : undefined}>
             <span className="player">
                 { !isEditing ? <span className="player-name">{playerName}</span> : <input type="text" value={playerName} onChange={changeHandler} required/> }
                 <span className="player-symbol">{symbol}</span>
