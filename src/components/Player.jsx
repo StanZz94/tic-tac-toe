ef8fbef63b263e5537c3d43d484ca9a1502a3a7d
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2024-11-14 23:35:39 +0200

    show name on win

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,30 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol, isActive }) {
+export default function Player({ name, symbol, isActive, onChangeName }) {
 
-    const [ playerName, setPlayerName ] = useState(name)
-    const [ isEditing, setIsEditing ] = useState(false);
+    const [playerName, setPlayerName] = useState(name)
+    const [isEditing, setIsEditing] = useState(false);
 
     function editHandler() {
         setIsEditing(editing => !editing);
 
+        if (isEditing) {
+            onChangeName(symbol, playerName)
+        }
     }
 
     function changeHandler(e) {
         setPlayerName(e.target.value);
 
     }
-    
+
     return (
         <li className={isActive ? "active" : undefined}>
             <span className="player">
-                { !isEditing ? <span className="player-name">{playerName}</span> : <input type="text" value={playerName} onChange={changeHandler} required/> }
+                {!isEditing ? <span className="player-name">{playerName}</span> : <input type="text" value={playerName} onChange={changeHandler} required />}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={editHandler}>{ isEditing ? "Save" : "Edit"}</button>
+            <button onClick={editHandler}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 }
